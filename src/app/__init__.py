40610f5e609d7550a1ddaee32a4d36159ccd0caf
UselesssCat <arie.cbpro@gmail.com>
2020-01-29 22:59:35 -0300

    Add partners endpoints

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -5,8 +5,10 @@ from flask import Flask
 
 def register_blueprints(app: Flask) -> None:
     from app.views.meta import meta_blueprint
+    from app.views.partners import partners_blueprint
 
     app.register_blueprint(meta_blueprint)
+    app.register_blueprint(partners_blueprint)
 
 
 def create_app() -> Flask:
---
+++ src/app/views/partners.py
@@ -0,0 +1,13 @@
+from flask import Blueprint
+
+partners_blueprint = Blueprint('partners', __name__, url_prefix='/partners')
+
+
+@partners_blueprint.route('/<int:id>', methods=['GET'])
+def get_partner(id: int) -> tuple:
+    return 'get_by_id', 200
+
+
+@partners_blueprint.route('/', methods=['POST'])
+def create_partner() -> tuple:
+    return 'create', 200
