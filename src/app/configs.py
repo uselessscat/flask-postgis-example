e88becf60fe2c733a196ced01629121a50d4ef24
UselesssCat <arie.cbpro@gmail.com>
2020-01-30 22:37:35 -0300

    Fix environment variable value

--- src/app/configs.py
+++ src/app/configs.py
@@ -12,6 +12,6 @@ class DefaultConfig:
 class DevelopmentConfig(DefaultConfig):
     DEBUG = os.environ.get('FLASK_DEBUG')
 
-    SQLALCHEMY_ECHO = os.environ.get('FLASK_DEBUG')
+    SQLALCHEMY_ECHO = os.environ.get('FLASK_DEBUG') == 'True'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
